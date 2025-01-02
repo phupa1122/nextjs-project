4b9a2d453bf1237e516493bd1cc3288e0196565a
View0202 <146795715+View0202@users.noreply.github.com>
2025-01-02 12:10:45 +0700

    response contact

---
+++ src/app/contact/map.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function map() {
+    return (
+        <div className="flex flex-row items-center justify-center px-20 my-5">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1893.6362344358247!2d98.8170656012571!3d18.334849968781175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30dbb903de457b15%3A0xade4cc5e472c4fdf!2sSS%20Com%20Service!5e0!3m2!1sth!2sth!4v1735791102936!5m2!1sth!2sth"
+                width="100%"
+                height="300"
+                style={{ border: 0, borderRadius: '6px' }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+        </div>
+    )
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Map from './map';
+
+import { PiPhoneCallFill } from "react-icons/pi";
+import { PiMapPinAreaFill } from "react-icons/pi";
 
 export default function contact() {
 
@@ -23,26 +27,28 @@ export default function contact() {
             </div>
 
             <div className='m-10'>
-                <div className="flex items-center justify-between px-20">
-                    <div className="text-start w-80 flex flex-col gap-3">
-                        <h1 className="text-2xl font-bold text-black1">
+                <div className="grid sm:grid-cols-1 md:flex md:items-center md:justify-between mx-20 gap-4">
+                    <div className="text-start w-full sm:w-full md:w-2/6 flex flex-col gap-1 bg-slate-50 rounded-md p-3">
+                        <h1 className="flex flex-row gap-2 text-xl font-bold text-black1 items-center">
+                            <PiPhoneCallFill size={24} />
                             เบอร์โทรศัพท์
                         </h1>
-                        <p className="text-base text-start text-black1">
+                        <p className="text-base text-start text-gray1">
                             090-3166790
                         </p>
                     </div>
-                    <div className="text-start w-80 flex flex-col gap-3">
-                        <h1 className="text-2xl font-bold text-black1">
+                    <div className="text-start w-full sm:w-full md:w-3/6 flex flex-col gap-1 bg-slate-50 rounded-md p-3">
+                        <h1 className="flex flex-row gap-2 text-xl font-bold text-black1 items-center">
+                            <PiMapPinAreaFill size={24} />
                             ที่อยู่
                         </h1>
-                        <p className="text-base text-start text-black1">
+                        <p className="text-base text-start text-gray1">
                             199 2 ตำบล บ้านโฮ่ง อำเภอ บ้านโฮ่ง ลำพูน 51130
                         </p>
                     </div>
                 </div>
-                <div className="flex flex-row items-center justify-center space-x-0 px-20 my-5">
-                </div>
+
+                <Map />
 
             </div>
         </>

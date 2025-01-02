a7e66b584368ddc18792baa08bde851518a3fa5b
View0202 <146795715+View0202@users.noreply.github.com>
2025-01-02 21:23:06 +0700

    response about

--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -23,42 +23,58 @@ export default function employee() {
             </div>
             <div className='m-10'>
                 <div className="flex flex-row items-center justify-center space-x-0 px-20">
-                    <div className="w-1/2 h-[400px] flex-row bg-white1 bg-center">
-                        <div className="h-[400px] flex-row bg-white1 bg-center" style={{ backgroundImage: "url('/images/About.png')" }}>
+                    <div className="w-1/2 h-[300px] flex-row bg-slate-50 bg-center rounded-l-lg">
+                        <div className="h-[300px] flex-row bg-slate-50 bg-center rounded-l-lg" style={{ backgroundImage: "url('/images/About.png')" }}>
 
                         </div>
                     </div>
-                    <div className="w-1/2 h-[400px] flex-row bg-[#FFFFFF] bg-center">
-                        <div className="flex items-center justify-center my-14">
+                    <div className="w-1/2 h-[300px] flex-row bg-slate-50 bg-center rounded-r-lg">
+                        <div className="flex items-center justify-center mx-5 my-11">
                             <div className="text-center w-80 flex flex-col gap-y-5">
-                                <p className="text-base text-black1 ">
+                                <p className="text-sm text-black1 ">
                                     พนักงานของเรา
                                 </p>
                                 <h1 className="text-2xl	 font-bold text-black1">
-                                แมว
+                                    แมว
                                 </h1>
-                                <p className="text-base text-start text-black1">
-                                มีประสบการณ์การทำงานเป็นแพทย์ความงาม 10 ปี ดูแลเคสปรับรูปหน้าหลากหลายรูปแบบเป็นเบื้องหลังการออกแบบคอร์สอบรมแพทย์ภายในองค์กรให้กับคลินิกหลายแห่ง
-                                    ทั่วประเทศ
+                                <p
+                                    className="text-sm text-black1 text-start"
+                                    style={{
+                                        display: "-webkit-box",
+                                        WebkitBoxOrient: "vertical",
+                                        WebkitLineClamp: 4,
+                                        overflow: "hidden",
+                                    }}
+                                >
+                                    มีประสบการณ์การทำงานเป็นแพทย์ความงาม 10 ปี ดูแลเคสปรับรูปหน้าหลากหลายรูปแบบเป็นเบื้องหลังการออกแบบคอร์สอบรมแพทย์ภายในองค์กรให้กับคลินิกหลายแห่งทั่วประเทศ
                                 </p>
-                                
+                                <p className="text-sm text-black1 ">
+                                    095-6789805
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
 
-
                 <div className="flex flex-row items-center justify-center space-x-0 px-20 my-10">
-                    <div className="w-1/2 h-[400px] flex-row bg-[#FFFFFF] bg-center">
-                        <div className="flex items-center justify-center my-14">
+                    <div className="w-1/2 h-[300px] flex-row bg-slate-50 bg-center rounded-l-lg">
+                        <div className="flex items-center justify-center mx-5 my-11">
                             <div className="text-center w-80 flex flex-col gap-y-5">
-                                <p className="text-base text-black1 ">
-                                พนักงานของเรา
+                                <p className="text-sm text-black1 ">
+                                    พนักงานของเรา
                                 </p>
                                 <h1 className="text-2xl	 font-bold text-black1">
                                     ชาย
                                 </h1>
-                                <p className="text-base text-start text-black1">
+                                <p
+                                    className="text-sm text-black1 text-start"
+                                    style={{
+                                        display: "-webkit-box",
+                                        WebkitBoxOrient: "vertical",
+                                        WebkitLineClamp: 4,
+                                        overflow: "hidden",
+                                    }}
+                                >
                                     มีประสบการณ์การทำงานเป็นแพทย์ความงาม 10 ปี ดูแลเคสปรับรูปหน้าหลากหลายรูปแบบเป็นเบื้องหลังการออกแบบคอร์สอบรมแพทย์ภายในองค์กรให้กับคลินิกหลายแห่ง
                                     ทั่วประเทศ
                                 </p>
@@ -68,30 +84,42 @@ export default function employee() {
                             </div>
                         </div>
                     </div>
-                    <div className="w-1/2 h-[400px] flex-row bg-white1 bg-center">
-                        <div className="h-[400px] flex-row bg-white1 bg-center" style={{ backgroundImage: "url('/images/About.png')" }}>
+                    <div className="w-1/2 h-[300px] flex-row bg-slate-50 bg-center rounded-r-lg">
+                        <div className="h-[300px] flex-row bg-slate-50 bg-center rounded-r-lg" style={{ backgroundImage: "url('/images/About.png')" }}>
                         </div>
                     </div>
                 </div>
 
                 <div className="flex flex-row items-center justify-center space-x-0 px-20">
-                    <div className="w-1/2 h-[400px] flex-row bg-white1 bg-center">
-                        <div className="h-[400px] flex-row bg-white1 bg-center" style={{ backgroundImage: "url('/images/About.png')" }}>
+                    <div className="w-1/2 h-[300px] flex-row bg-slate-50 bg-center rounded-l-lg">
+                        <div className="h-[300px] flex-row bg-slate-50 bg-center rounded-l-lg" style={{ backgroundImage: "url('/images/About.png')" }}>
 
                         </div>
                     </div>
-                    <div className="w-1/2 h-[400px] flex-row bg-[#FFFFFF] bg-center">
-                        <div className="flex items-center justify-center my-14">
+                    <div className="w-1/2 h-[300px] flex-row bg-slate-50 bg-center rounded-r-lg">
+                        <div className="flex items-center justify-center mx-5 my-11">
                             <div className="text-center w-80 flex flex-col gap-y-5">
                                 <p className="text-base text-black1 ">
-                                พนักงานของเรา
+                                    พนักงานของเรา
                                 </p>
                                 <h1 className="text-2xl	 font-bold text-black1">
-                                    หญิง                                </h1>
-                                <p className="text-base text-start text-black1">
+                                    หญิง
+                                </h1>
+                                <p
+                                    className="text-sm text-black1 text-start"
+                                    style={{
+                                        display: "-webkit-box",
+                                        WebkitBoxOrient: "vertical",
+                                        WebkitLineClamp: 4,
+                                        overflow: "hidden",
+                                    }}
+                                >
                                     เรามุ่งมั่นที่จะใช้เฉพาะส่วนผสมที่ดีที่สุดในผลิตภัณฑ์ของเราโดยเน้นที่สูตรที่เป็นธรรมชาติและปราศจาก
                                     การทดลองกับสัตว์ทีมผู้เชี่ยวชาญของเราทำงาน
                                     อย่างไม่รู้จักเหน็ดเหนื่อยเพื่อพัฒนาผลิตภัณฑ์นวัตกรรมที่ให้ผลลัพธ์ที่แท้จริงตั้งแต่ผลิตภัณฑ์ดูแลผิวขั้นพื้นฐานไปจนถึงเครื่องสำอางสีสันสดใส
+                                </p>
+                                <p className="text-sm text-black1 ">
+                                    095-6789805
                                 </p>
                             </div>
                         </div>

a8233c688d199a9b995374ff36304aebd521f392
View0202 <146795715+View0202@users.noreply.github.com>
2025-01-05 18:30:19 +0700

    filter productAndService

--- src/app/productAndService/page.tsx
+++ src/app/productAndService/page.tsx
@@ -12,26 +12,26 @@ import { IoMdArrowDropup } from "react-icons/io";
 import { PiShoppingBagOpenFill } from "react-icons/pi";
 
 const data = [
-    { id: "1", name: "สินค้า", type: "ครีม", title: "หน้าขาว", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
-    { id: "2", name: "สินค้า", type: "ครีม", title: "ใสสะอาด", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
-    { id: "3", name: "สินค้า", type: "ครีม", title: "ลดสิว", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
-    { id: "4", name: "สินค้า", type: "ครีม", title: "ผิวเนียน", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
-    { id: "5", name: "สินค้า", type: "แชมพู", title: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
-    { id: "6", name: "สินค้า", type: "แชมพู", title: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
-    { id: "7", name: "สินค้า", type: "แชมพู", title: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
-    { id: "8", name: "สินค้า", type: "แชมพู", title: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
-    { id: "9", name: "ทรีตเมนต์", type: "ทรีตเมนต์", title: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
-    { id: "10", name: "ทรีตเมนต์", type: "ทรีตเมนต์", title: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
-    { id: "11", name: "ทรีตเมนต์", type: "ทรีตเมนต์", title: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
-    { id: "12", name: "ทรีตเมนต์", type: "ทรีตเมนต์", title: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
-    { id: "13", name: "สักคิ้ว", type: "สักคิ้ว", title: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ3", price: "150฿", image: "/images/product/Tattoo.png" },
-    { id: "14", name: "สักคิ้ว", type: "สักคิ้ว", title: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ4", price: "150฿", image: "/images/product/Tattoo.png" },
-    { id: "15", name: "สักคิ้ว", type: "สักคิ้ว", title: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ5", price: "150฿", image: "/images/product/Tattoo.png" },
-    { id: "16", name: "สักคิ้ว", type: "สักคิ้ว", title: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ", price: "150฿", image: "/images/product/Tattoo.png" },
-    { id: "17", name: "ทำเล็บ", type: "ทำเล็บ", title: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
-    { id: "18", name: "ทำเล็บ", type: "ทำเล็บ", title: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
-    { id: "19", name: "ทำเล็บ", type: "ทำเล็บ", title: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
-    { id: "20", name: "ทำเล็บ", type: "ทำเล็บ", title: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
+    { id: "1", type: "สินค้า", title: "ครีม", name: "หน้าขาว", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
+    { id: "2", type: "สินค้า", title: "ครีม", name: "ใสสะอาด", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
+    { id: "3", type: "สินค้า", title: "ครีม", name: "ลดสิว", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
+    { id: "4", type: "สินค้า", title: "ครีม", name: "ผิวเนียน", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/Product.png" },
+    { id: "5", type: "สินค้า", title: "แชมพู", name: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
+    { id: "6", type: "สินค้า", title: "แชมพู", name: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
+    { id: "7", type: "สินค้า", title: "แชมพู", name: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
+    { id: "8", type: "สินค้า", title: "แชมพู", name: "แชมพู", subtitle: "ที่สุดแห่งการฟื้นบำรุงทุกปัญหา", price: "299฿", image: "/images/product/shampoo.png" },
+    { id: "9", type: "ทรีตเมนต์", title: "ทรีตเมนต์", name: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
+    { id: "10", type: "ทรีตเมนต์", title: "ทรีตเมนต์", name: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
+    { id: "11", type: "ทรีตเมนต์", title: "ทรีตเมนต์", name: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
+    { id: "12", type: "ทรีตเมนต์", title: "ทรีตเมนต์", name: "ทรีตเมนต์หน้า", subtitle: "ใบหน้าดูกระจ่างใสขึ้น เสริมให้ผิวแข็งแรง", price: "350฿", image: "/images/product/Treatment.png" },
+    { id: "13", type: "สักคิ้ว", title: "สักคิ้ว", name: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ3", price: "150฿", image: "/images/product/Tattoo.png" },
+    { id: "14", type: "สักคิ้ว", title: "สักคิ้ว", name: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ4", price: "150฿", image: "/images/product/Tattoo.png" },
+    { id: "15", type: "สักคิ้ว", title: "สักคิ้ว", name: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ5", price: "150฿", image: "/images/product/Tattoo.png" },
+    { id: "16", type: "สักคิ้ว", title: "สักคิ้ว", name: "สักคิ้ว", subtitle: "การสักคิ้ว 3 มิติหรือสักคิ้ว 6 มิติ", price: "150฿", image: "/images/product/Tattoo.png" },
+    { id: "17", type: "ทำเล็บ", title: "ทำเล็บ", name: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
+    { id: "18", type: "ทำเล็บ", title: "ทำเล็บ", name: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
+    { id: "19", type: "ทำเล็บ", title: "ทำเล็บ", name: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
+    { id: "20", type: "ทำเล็บ", title: "ทำเล็บ", name: "ทำเล็บ", subtitle: "ฝีมือช่างขั้นเทพ ราคาถูก", price: "199฿", image: "/images/product/Nail.png" },
 ];
 
 const PriceRangeSlider = () => {
@@ -95,14 +95,14 @@ export default function productAndService() {
 
     useEffect(() => {
         const filtered = data.filter((item) => {
-            return item.title.includes(searchText);
+            return item.name.includes(searchText);
         });
         setFilteredProductAndServices(filtered);
     }, [searchText]);
 
     const handleCream = () => {
         const filtered = data.filter((item) => {
-            return item.type === 'ครีม';
+            return item.title === 'ครีม';
             //router.push('/productAndService/product_cream');
         });
         setFilteredProductAndServices(filtered);
@@ -110,7 +110,7 @@ export default function productAndService() {
     };
     const handleShampoo = () => {
         const filtered = data.filter((item) => {
-            return item.type === 'แชมพู';
+            return item.title === 'แชมพู';
         });
         setFilteredProductAndServices(filtered);
         console.log(filtered);
@@ -118,21 +118,21 @@ export default function productAndService() {
 
     const handleTreatment = () => {
         const filtered = data.filter((item) => {
-            return item.name === 'ทรีตเมนต์';
+            return item.title === 'ทรีตเมนต์';
         });
         setFilteredProductAndServices(filtered);
         console.log(filtered);
     };
     const handleTattoo = () => {
         const filtered = data.filter((item) => {
-            return item.name === 'สักคิ้ว';
+            return item.title === 'สักคิ้ว';
         });
         setFilteredProductAndServices(filtered);
         console.log(filtered);
     };
     const handleNail = () => {
         const filtered = data.filter((item) => {
-            return item.name === 'ทำเล็บ';
+            return item.title === 'ทำเล็บ';
         });
         setFilteredProductAndServices(filtered);
         console.log(filtered);
@@ -277,7 +277,7 @@ export default function productAndService() {
                                             ).map((item, index) => (
                                                 <div className="bg-[#ffffff] p-4 w-32 lg:w-72 md:w-72 drop-shadow-md rounded-lg mx-auto" key={index}>
                                                     <div className="w-full space-y-2">
-                                                        <h1 className="text-black1 text-2xl text-center font-bold">{item.title}</h1>
+                                                        <h1 className="text-black1 text-2xl text-center font-bold">{item.name}</h1>
                                                         <div className="mx-auto w-44 h-44 rounded-md overflow-hidden mb-4">
                                                             <Image
                                                                 src={item.image}
@@ -307,7 +307,7 @@ export default function productAndService() {
                                                         </div>
 
                                                         <div className="text-center mt-4 border-t border-gray1-300 pt-4">
-                                                            {item.name === "สินค้า" ? (
+                                                            {item.type === "สินค้า" ? (
                                                                 <p className="text-gray1">สามารถซื้อได้ที่หน้าร้านเท่านั้น</p>
                                                             ) : (
                                                                 <ModalBooking />
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const posts = [
     {
         id: 1,
-        title: 'Boost your conversion rate',
+        title: 'ใบหน้ากระฉับมีสัดส่วน',
         href: '#',
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
+        date: '16 ธ.ค 2567',
+        datetime: '2024-12-16',
+        category: { title: 'ทรีตเมนต์', href: '#' },
         author: {
             name: 'Michael Foster',
             role: 'Co-Founder / CTO',
@@ -47,43 +47,43 @@ export default function result() {
 
             <div className="m-10">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl lg:mx-0">
+                    {/* <div className="mx-auto max-w-2xl lg:mx-0">
                         <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray1-900 sm:text-5xl">From the blog</h2>
                         <p className="mt-2 text-lg/8 text-gray1-600">Learn how to grow your business with our expert advice.</p>
-                    </div>
-                    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray1-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    </div> */}
+                    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray1-200 pt-10 sm:mt-5 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         {posts.map((post) => (
-                            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+                            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between bg-slate-50 p-5 rounded-md">
                                 <div className="flex items-center gap-x-4 text-xs">
-                                    <time dateTime={post.datetime} className="text-gray1-500">
+                                    <time dateTime={post.datetime} className="text-gray1">
                                         {post.date}
                                     </time>
                                     <a
                                         href={post.category.href}
-                                        className="relative z-10 rounded-full bg-gray1-50 px-3 py-1.5 font-medium text-gray1-600 hover:bg-gray1-100"
+                                        className="relative z-10 rounded-full bg-gray1-50 px-3 py-1.5 font-medium text-gray1 hover:bg-gray1"
                                     >
                                         {post.category.title}
                                     </a>
                                 </div>
                                 <div className="group relative">
-                                    <h3 className="mt-3 text-lg/6 font-semibold text-gray1-900 group-hover:text-gray1-600">
+                                    <h3 className="mt-3 text-lg/6 font-semibold text-gray1 group-hover:text-gray1">
                                         <a href={post.href}>
                                             <span className="absolute inset-0" />
                                             {post.title}
                                         </a>
                                     </h3>
-                                    <p className="mt-5 line-clamp-3 text-sm/6 text-gray1-600">{post.description}</p>
+                                    <p className="mt-5 line-clamp-3 text-sm/6 text-gray1">{post.description}</p>
                                 </div>
                                 <div className="relative mt-8 flex items-center gap-x-4">
-                                    <img alt="" src={post.author.imageUrl} className="size-10 rounded-full bg-gray1-50" />
+                                    <img alt="" src={post.author.imageUrl} className="size-10 rounded-full bg-gray1" />
                                     <div className="text-sm/6">
-                                        <p className="font-semibold text-gray1-900">
+                                        <p className="font-semibold text-gray1">
                                             <a href={post.author.href}>
                                                 <span className="absolute inset-0" />
                                                 {post.author.name}
                                             </a>
                                         </p>
-                                        <p className="text-gray1-600">{post.author.role}</p>
+                                        <p className="text-gray1">{post.author.role}</p>
                                     </div>
                                 </div>
                             </article>

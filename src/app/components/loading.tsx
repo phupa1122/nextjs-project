0d81cbb56ce885f305825f800fafb3854607095c
View0202 <146795715+View0202@users.noreply.github.com>
2025-01-10 11:53:06 +0700

    loading

---
+++ src/app/components/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function loading() {
+    return (
+        <div className="flex justify-center items-center gap-3 p-40">
+            <span className="loading loading-spinner text-pink1 w-16 h-16"></span>
+
+            <p className="text-black1 font-medium text-lg">กำลังโหลด...</p>
+        </div>
+    )
+}
